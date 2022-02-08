3b3d273af3cd57782e60b968806ff59a98397cd1
siwooyong <68500343+siwooyong@users.noreply.github.com>
2022-02-08 21:28:25 +0900

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -28,38 +28,3 @@ for i in range(len(train_json)):
 label_encoder = sorted(np.unique(labels))
 label_encoder = {key:value for key,value in zip(label_encoder, range(len(label_encoder)))}
 label_decoder = {val:key for key, val in label_encoder.items()}
-
-
-# cutmix function
-
-def rand_bbox(size, lam):
-    W = size[2]
-    H = size[3]
-    cut_rat = np.sqrt(1. - lam)
-    cut_w = np.int(W * cut_rat)
-    cut_h = np.int(H * cut_rat)
-
-    # uniform
-    cx = np.random.randint(W)
-    cy = np.random.randint(H)
-
-    bbx1 = np.clip(cx - cut_w // 2, 0, W)
-    bby1 = np.clip(cy - cut_h // 2, 0, H)
-    bbx2 = np.clip(cx + cut_w // 2, 0, W)
-    bby2 = np.clip(cy + cut_h // 2, 0, H)
-    return bbx1, bby1, bbx2, bby2
-
-def cutmix(data, target, alpha):
-    indices = torch.randperm(data.size(0))
-    shuffled_data = data[indices]
-    shuffled_target = target[indices]
-
-    lam = np.clip(np.random.beta(alpha, alpha),0.3,0.4)
-    bbx1, bby1, bbx2, bby2 = rand_bbox(data.size(), lam)
-    new_data = data.clone()
-    new_data[:, :, bby1:bby2, bbx1:bbx2] = data[indices, :, bby1:bby2, bbx1:bbx2]
-    # adjust lambda to exactly match pixel ratio
-    lam = 1 - ((bbx2 - bbx1) * (bby2 - bby1) / (data.size()[-1] * data.size()[-2]))
-    targets = (target, shuffled_target, lam)
-
-    return new_data, targets
